97c084ac1f74d327c9c931edb39b9897e46fc8ad
neb <jneb.dev@outlook.com>
2021-06-29 22:04:18 -0600

    navigate to best selling televisions and screenshot

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+
+const url = "https://www.amazon.com/";
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
+  // navigating to amazon's homepage
+  await page.goto(url);
+
+  // clicking "best sellers" in primary nav
+  await page.waitForSelector("div#nav-xshop");
+  await page.click(
+    'a[data-csa-c-content-id="nav_cs_bestsellers_8a080d3d7b55497ea1bdd97b7cff8b7b"]'
+  );
+
+  // clicking "electronics" in side nav
+  await page.waitForSelector("ul#zg_browseRoot");
+  await page.click(
+    'ul#zg_browseRoot a[href="https://www.amazon.com/Best-Sellers-Electronics/zgbs/electronics/ref=zg_bs_nav_0"]'
+  );
+
+  // clicking "Televisions & Video" in side nav
+  await page.waitForSelector("ul#zg_browseRoot");
+  await page.click(
+    'ul#zg_browseRoot a[href="https://www.amazon.com/Best-Sellers-Electronics-Televisions-Video-Products/zgbs/electronics/1266092011/ref=zg_bs_nav_e_1_e"]'
+  );
+
+  // clicking "Televisions" in side nav
+  await page.waitForSelector("ul#zg_browseRoot");
+  await page.click(
+    'ul#zg_browseRoot a[href="https://www.amazon.com/Best-Sellers-Electronics-Televisions/zgbs/electronics/172659/ref=zg_bs_nav_e_2_1266092011"]'
+  );
+
+  // taking screenshot of best sellers in televisions
+  await page.waitForSelector("#zg-ordered-list", { visible: true });
+  await page.screenshot({ path: "televisions.png", fullPage: true });
 
   await browser.close();
 })();
